4a9e2d2ef95993928fcc9d5bc07fe1e286f3ac38
J-joon <jjkim030309@gmail.com>
2026-04-10 11:59:16 +0900

    fix: wrong script structure

--- src/jscript/print_tasks.py
+++ src/jscript/print_tasks.py
@@ -3,10 +3,13 @@ import tyro
 import pandas as pd
 
 DEFAULT_PATH = Path("~/.cache/huggingface/lerobot")
-def main(repo_id: str):
+def run(repo_id: str):
     path = DEFAULT_PATH.expanduser() / repo_id / "meta" / "tasks.parquet"
     df = pd.read_parquet(path)
     print(df)
 
+def main():
+    tyro.cli(run)
+
 if __name__ == "__main__":
-    tyro.cli(main)
+    main()
